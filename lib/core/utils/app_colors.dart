6563c8f56e8e1afb59fd02158887cf24614480e0
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-21 10:02:51 +0200

    Add assets and utility files for app styling and images
    Added various font files and image assets to the project. Also, created utility files for managing colors, images, and text styles to ensure consistent styling across the application. Updated `pubspec.yaml` to include the new font assets. Modified `main.dart` to set up the basic structure of the app using a new `AspenApp` class.

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color mainColorTheme = Colors.black;
+  static const Color secondaryColorTheme = Colors.white;
+  static const Color mainColor = Color(0xffae1f24);
+  static const Color secondaryColor = Color(0xff666666);
+}
---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.png
+  static const String imagesAppLogo = "assets/images/app_logo.png";
+
+  /// Assets for imagesBanuo
+  /// assets/images/banuo.svg
+  static const String imagesBanuo = "assets/images/banuo.svg";
+
+  /// Assets for imagesCityOne
+  /// assets/images/city_one.png
+  static const String imagesCityOne = "assets/images/city_one.png";
+
+  /// Assets for imagesCityTwo
+  /// assets/images/city_two.png
+  static const String imagesCityTwo = "assets/images/city_two.png";
+
+  /// Assets for imagesKitchen
+  /// assets/images/kitchen.svg
+  static const String imagesKitchen = "assets/images/kitchen.svg";
+
+  /// Assets for imagesPool
+  /// assets/images/pool.svg
+  static const String imagesPool = "assets/images/pool.svg";
+
+  /// Assets for imagesPopularOne
+  /// assets/images/popular_one.png
+  static const String imagesPopularOne = "assets/images/popular_one.png";
+
+  /// Assets for imagesPopularTwo
+  /// assets/images/popular_two.png
+  static const String imagesPopularTwo = "assets/images/popular_two.png";
+
+  /// Assets for imagesSplashBackground
+  /// assets/images/splash_background.png
+  static const String imagesSplashBackground =
+      "assets/images/splash_background.png";
+
+  /// Assets for imagesWifi
+  /// assets/images/wifi.svg
+  static const String imagesWifi = "assets/images/wifi.svg";
+}
