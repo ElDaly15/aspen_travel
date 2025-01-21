03dceb5be6613952a37e103f46949a808ef56e61
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-21 11:12:57 +0200

    Enable Device Preview for easier UI testing
    Added the `device_preview` package to the project and configured it in the `main.dart` file to allow for easier UI testing across different device frames. Also, updated the `GeneratedPluginRegistrant.swift` to include the `SharedPreferencesPlugin` registration, which seems to be a necessary change for the `shared_preferences` package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:aspen/featuers/splash/presentation/views/splah_view.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
-  runApp(const AspenApp());
+  runApp(DevicePreview(enabled: true, builder: (context) => const AspenApp()));
 }
 
 class AspenApp extends StatelessWidget {
