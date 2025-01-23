35f22744617804649fdfa3d901dec2515a41bb41
Mazen Eldaly <dalydevelopment15@gmail.com>
2025-01-23 11:00:16 +0200

    Add RowOfShowMap widget and enhance CardDetailsViewBody
    Added a new widget `RowOfShowMap` to display location details and a "Show Map" button. Enhanced `CardDetailsViewBody` to include this new widget, added a "Read More" button for the description text, and improved the overall layout and styling.

--- lib/featuers/card_details/presentation/views/widgets/card_details_view_body.dart
+++ lib/featuers/card_details/presentation/views/widgets/card_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:aspen/core/utils/app_styles.dart';
+import 'package:aspen/featuers/card_details/presentation/views/widgets/row_of_show_map.dart';
 import 'package:aspen/featuers/card_details/presentation/views/widgets/stack_of_image_and_container.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +11,72 @@ class CardDetailsViewBody extends StatefulWidget {
 }
 
 class _CardDetailsViewBodyState extends State<CardDetailsViewBody> {
+  int? maxLines = 5;
+  bool switchBetweenSeeMore = false;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 22),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 22),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SafeArea(
+          const SafeArea(
             child: SizedBox(
               height: 10,
             ),
           ),
-          ImageStackAndContainer(),
+          const ImageStackAndContainer(),
+          const SizedBox(
+            height: 20,
+          ),
+          const RowOfShowMap(),
+          const SizedBox(
+            height: 14,
+          ),
+          Text(
+            'Aspen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping andAspen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping anpen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping andAspen is as close as one can get to a storybook alpine town in America. The choose-your-own-adventure possibilities—skiing, hiking, dining shopping a',
+            style: TextStyles.font18Medium(context).copyWith(
+              color: const Color(0xff606060),
+            ),
+            maxLines: maxLines,
+            overflow: switchBetweenSeeMore
+                ? TextOverflow.visible
+                : TextOverflow.ellipsis,
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                switchBetweenSeeMore = !switchBetweenSeeMore;
+                maxLines = switchBetweenSeeMore ? null : 5;
+              });
+            },
+            style: ButtonStyle(
+              padding: WidgetStateProperty.all(
+                const EdgeInsets.symmetric(vertical: 0),
+              ),
+            ),
+            child: SizedBox(
+              width: 120,
+              child: Row(
+                children: [
+                  Text(
+                    'Read More',
+                    style: TextStyles.font18Bold(context).copyWith(
+                      color: const Color(0xff176FF2),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Icon(
+                    Icons.keyboard_arrow_down_outlined,
+                    color: Color(0xff176FF2),
+                    size: 25,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/featuers/card_details/presentation/views/widgets/row_of_show_map.dart
@@ -0,0 +1,50 @@
+import 'package:aspen/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class RowOfShowMap extends StatelessWidget {
+  const RowOfShowMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Coeurdes Alpes',
+              style: TextStyles.font24SemiBold(context).copyWith(fontSize: 28),
+            ),
+            Row(
+              children: [
+                const Icon(
+                  Icons.star,
+                  color: Color(0xffF8D675),
+                  size: 22,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '4.5 (123 Reviews)',
+                  style: TextStyles.font14SemiBold(context)
+                      .copyWith(color: const Color(0xff606060)),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const Spacer(),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            'Show Map',
+            style: TextStyles.font18Bold(context).copyWith(
+              color: const Color(0xff176FF2),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
